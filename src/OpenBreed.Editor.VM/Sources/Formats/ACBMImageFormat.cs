c9b99c4959d7361e9cf279324b5288f6c2ccb735
mrpetro <petro.silowy@gmail.com>
2018-12-09 22:53:42 +0100

    Updated ACBMImageReader and ABHC ABSE database image entries

--- src/OpenBreed.Editor.VM/Sources/Formats/ACBMImageFormat.cs
+++ src/OpenBreed.Editor.VM/Sources/Formats/ACBMImageFormat.cs
@@ -20,12 +20,24 @@ namespace OpenBreed.Editor.VM.Sources.Formats
             int width = source.GetParameter<int>("WIDTH");
             int height = source.GetParameter<int>("HEIGHT");
             int bitPlanesNo = source.GetParameter<int>("BIT_PLANES_NO");
+            var paletteStr = source.GetParameter<string>("PALETTE_MODE");
+
+            ACBMImageReader.ACBMPaletteMode paletteMode;
+
+            if (paletteStr == "NONE")
+                paletteMode = ACBMImageReader.ACBMPaletteMode.NONE;
+            else if (paletteStr == "16BIT")
+                paletteMode = ACBMImageReader.ACBMPaletteMode.PALETTE_16BIT;
+            else if (paletteStr == "32BIT")
+                paletteMode = ACBMImageReader.ACBMPaletteMode.PALETTE_32BIT;
+            else
+                throw new InvalidOperationException(paletteStr);
 
             //Remember to set source stream to begining
             source.Stream.Seek(0, SeekOrigin.Begin);
 
             var imageBuilder = ImageBuilder.NewImage();
-            var reader = new ACBMImageReader(imageBuilder, width, height, bitPlanesNo, true);
+            var reader = new ACBMImageReader(imageBuilder, width, height, bitPlanesNo, paletteMode);
             return reader.Read(source.Stream);
         }
 
