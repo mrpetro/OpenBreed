dcaafcedb12b9eb7a71ce52c6c4a4b67c158827d
mrpetro <petro.silowy@gmail.com>
2019-03-15 22:06:47 +0100

    Refactoring assets

--- src/OpenBreed.Editor.VM/Palettes/PaletteFromMapVM.cs
+++ src/OpenBreed.Editor.VM/Palettes/PaletteFromMapVM.cs
@@ -81,7 +81,7 @@ namespace OpenBreed.Editor.VM.Palettes
 
                 var dataProvider = ServiceLocator.Instance.GetService<DataProvider>();
 
-                var map = dataProvider.Datas.GetData(source.DataRef) as MapModel;
+                var map = dataProvider.GetData(source.DataRef) as MapModel;
 
                 if (map == null)
                     return;
@@ -130,7 +130,7 @@ namespace OpenBreed.Editor.VM.Palettes
 
         private void ToEntry(IPaletteFromMapEntry source)
         {
-            var mapModel = ServiceLocator.Instance.GetService<DataProvider>().Datas.GetData(DataRef) as MapModel;
+            var mapModel = ServiceLocator.Instance.GetService<DataProvider>().GetData(DataRef) as MapModel;
 
             var paletteBlock = mapModel.Blocks.OfType<MapPaletteBlock>().FirstOrDefault(item => item.Name == BlockName);
 
