1355dadb807423e7345ba21cdc39774ccf8d9438
mrpetro <petro.silowy@gmail.com>
2019-04-30 22:27:40 +0200

    Refactoring

---
+++ src/Code/OpenBreed.Game/Control/Components/MovementControl.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenBreed.Game.Common;
+using OpenBreed.Game.Entities;
+
+namespace OpenBreed.Game.Control.Components
+{
+    public enum MovementDirection
+    {
+        Right,
+        Up,
+        Left,
+        Down
+    }
+
+    public class MovementControl : IControlComponent
+    {
+        public Type SystemType { get { return typeof(ControlSystem); } }
+
+        public void Move(MovementDirection direction)
+        {
+
+        }
+
+        public void Deinitialize(IEntity entity)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void Initialize(IEntity entity)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Code/OpenBreed.Game/Control/ControlSystem.cs
@@ -0,0 +1,14 @@
+﻿using OpenBreed.Game.Common;
+using OpenBreed.Game.Control.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenBreed.Game.Control
+{
+    public class ControlSystem : WorldSystem<IControlComponent>
+    {
+    }
+}
