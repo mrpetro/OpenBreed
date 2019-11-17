49297793e93153713cf28bb682716e541a0aebd8
mrpetro <petro.silowy@gmail.com>
2019-11-17 14:02:52 +0100

    Moving ABTA Password encoder to OpenBreed.Common Updating .NET to version 4.7.2

--- src/OpenBreed.Editor.UI.WinForms/Forms/ABTAPasswordGeneratorForm.cs
+++ src/OpenBreed.Editor.UI.WinForms/Forms/ABTAPasswordGeneratorForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenBreed.Common.Tools;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -6,7 +7,6 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-using ToolsABTA;
 
 namespace OpenBreed.Editor.UI.WinForms.Forms
 {
