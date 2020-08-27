1f386c51353aea85c118cc1579d937b543689747
mrpetro <petro.silowy@gmail.com>
2020-08-27 22:36:55 +0200

    Refactor

---
+++ src/OpenBreed.Editor.VM/Database/Tables/DbScriptTableVM.cs
@@ -0,0 +1,34 @@
+﻿using OpenBreed.Common;
+using OpenBreed.Common.Texts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenBreed.Editor.VM.Database.Tables
+{
+    public class DbScriptTableVM : DbTableVM
+    {
+        #region Private Fields
+
+        private readonly IRepository<IScriptEntry> _repository;
+
+        #endregion Private Fields
+
+        #region Public Constructors
+
+        public DbScriptTableVM()
+        {
+        }
+
+        #endregion Public Constructors
+
+        #region Public Properties
+
+        public override string Name { get { return "Scripts"; } }
+
+        #endregion Public Properties
+
+    }
+}
