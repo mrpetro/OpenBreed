c99179474b8d9e4490e503eed7422fc825758029
mrpetro <petro.silowy@gmail.com>
2020-09-11 22:44:34 +0200

    Adding some sample lua scripts

---
+++ src/OpenBreed.Database.Xml/Defaults/Scripts/simpleEntry.lua
@@ -0,0 +1,42 @@
+﻿
+WorldLoaded = function(World world)
+	camera = Entities.GetByName("MainCamera")
+
+	logo = Entities.CreateFromTemplate("Logo1");
+	world.Add(logo);
+
+	Trigger.Add(Camera.Events.FadeIn, camera, Logo1FadedIn)
+	camera.FadeIn(DateTime.Seconds(1))
+end
+
+Logo1FadedIn = function()
+	Trigger.Remove(Logo1FadedIn)
+	world.Remove(logo);
+
+	Trigger.OnEvent(Camera.Events.FadeOut, camera, Logo1FadedOut)
+	camera.FadeOut(DateTime.Seconds(1))
+end
+
+Logo1FadedOut = function()
+	Trigger.Remove(Logo1FadedOut)
+	world.Remove(logo);
+
+    logo = Entities.CreateFromTemplate("Logo2");
+	world.Add(logo);
+
+	Trigger.OnEvent(Camera.Events.FadeIn, camera, Logo2FadedIn)
+	camera.FadeIn(DateTime.Seconds(1))
+end
+
+Logo2FadedIn = function()
+	Trigger.Remove(Logo2FadedIn)
+	world.Remove(logo);
+
+	Trigger.OnEvent(Camera.Events.FadeOut, camera, Logo2FadedOut)
+	camera.FadeOut(DateTime.Seconds(1))
+end
+
+Logo2FadedOut = function()
+	Trigger.Remove(Logo2FadedOut)
+	world.Remove(logo);
+end
---
+++ src/OpenBreed.Database.Xml/Defaults/Templates/Logo1.lua
@@ -0,0 +1,11 @@
+﻿Templates.Entities.Arrow = {
+    AnimationComponent = {
+        Speed = 10.0,
+        Loop = true},
+    PositionComponent = { 0, 0 },
+	SpriteComponent = {
+		AtlasId = "Atlases/Sprites/Arrow",
+        Order = 100.0,
+        Width = 32.0,
+        Height = 32.0}
+}
