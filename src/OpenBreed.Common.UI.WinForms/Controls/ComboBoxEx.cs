6e68b5291ac7aa7c0590767a53a771293d55689f
mrpetro <petro.silowy@gmail.com>
2020-10-03 07:47:42 +0200

    Refactoring

---
+++ src/OpenBreed.Common.UI.WinForms/Controls/ComboBoxEx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace OpenBreed.Common.UI.WinForms.Controls
+{
+    public class ComboBoxEx : ComboBox
+    {
+        #region Protected Methods
+
+        protected override void OnSelectedIndexChanged(EventArgs e)
+        {
+            //This will cause selected item value change instantly after index is changed
+            if (DataBindings.Count > 0)
+                DataBindings[0].WriteValue();
+
+            base.OnSelectedIndexChanged(e);
+        }
+
+        #endregion Protected Methods
+    }
+}
