975efd05d6e378da83b91814c023b92e788f1547
mrpetro <petro.silowy@gmail.com>
2020-11-01 23:07:04 +0100

    Refactoring

---
+++ src/OpenBreed.Common/Data/IDataProvider.cs
@@ -0,0 +1,18 @@
+﻿using OpenBreed.Database.Interface;
+using OpenBreed.Database.Interface.Items;
+using System;
+
+namespace OpenBreed.Common.Data
+{
+    public interface IDataProvider
+    {
+        #region Public Methods
+
+        IRepository<T> GetRepository<T>() where T : IEntry;
+        IRepository GetRepository(Type entryType);
+
+        bool TryGetData<T>(string id, out T item, out string message);
+        T GetData<T>(string id);
+        #endregion Public Methods
+    }
+}
