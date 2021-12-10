bc9354530bfc82a076b477f528174f21ec84db24
mrpetro <petro.silowy@gmail.com>
2021-12-11 00:47:07 +0100

    Converting OpenBreed.Common.Tools to use net standard 2.0

--- src/OpenBreed.Common.Tools/Sounds/PCMPlayer.cs
+++ src/OpenBreed.Common.Tools/Sounds/PCMPlayer.cs
@@ -1,47 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Media;
-using System.IO;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Media;
+//using System.IO;
 
-namespace OpenBreed.Common.Tools.Sounds
-{
-    public class PCMPlayer
-    {
-        private readonly SoundPlayer _soundPlayer;
+//namespace OpenBreed.Common.Tools.Sounds
+//{
+//    public class PCMPlayer
+//    {
+//        private readonly SoundPlayer _soundPlayer;
 
-        public PCMPlayer(byte[] pcmSampleBytes, int samplingRate, int bitsPerSample, int channels)
-        {
-            using (WaveMemoryStream waveStream = new WaveMemoryStream(pcmSampleBytes, samplingRate, bitsPerSample, channels))
-            {
-                _soundPlayer = new SoundPlayer(waveStream);
-            }
-        }
+//        public PCMPlayer(byte[] pcmSampleBytes, int samplingRate, int bitsPerSample, int channels)
+//        {
+//            using (WaveMemoryStream waveStream = new WaveMemoryStream(pcmSampleBytes, samplingRate, bitsPerSample, channels))
+//            {
+//                _soundPlayer = new SoundPlayer(waveStream);
+//            }
+//        }
 
-        public PCMPlayer(string pcmFilePath, int samplingRate, ushort bitsPerSample, ushort channels)
-        {
-            using (FileStream pcmSampleStream = File.Open(pcmFilePath, FileMode.Open))
-            {
-                byte[] pcmSampleBytes = null;
-                BinaryReader br = new BinaryReader(pcmSampleStream);
-                long numBytes = new FileInfo(pcmFilePath).Length;
-                pcmSampleBytes = br.ReadBytes((int)numBytes);
-                using (WaveMemoryStream waveStream = new WaveMemoryStream(pcmSampleBytes, samplingRate, bitsPerSample, channels))
-                {
-                    _soundPlayer = new SoundPlayer(waveStream);
-                }
-            }
-        }
+//        public PCMPlayer(string pcmFilePath, int samplingRate, ushort bitsPerSample, ushort channels)
+//        {
+//            using (FileStream pcmSampleStream = File.Open(pcmFilePath, FileMode.Open))
+//            {
+//                byte[] pcmSampleBytes = null;
+//                BinaryReader br = new BinaryReader(pcmSampleStream);
+//                long numBytes = new FileInfo(pcmFilePath).Length;
+//                pcmSampleBytes = br.ReadBytes((int)numBytes);
+//                using (WaveMemoryStream waveStream = new WaveMemoryStream(pcmSampleBytes, samplingRate, bitsPerSample, channels))
+//                {
+//                    _soundPlayer = new SoundPlayer(waveStream);
+//                }
+//            }
+//        }
 
-        public void Play()
-        {
-            _soundPlayer.Play();
-        }
+//        public void Play()
+//        {
+//            _soundPlayer.Play();
+//        }
 
-        public void PlaySync()
-        {
-            _soundPlayer.PlaySync();
-        }
-    }
-}
+//        public void PlaySync()
+//        {
+//            _soundPlayer.PlaySync();
+//        }
+//    }
+//}
