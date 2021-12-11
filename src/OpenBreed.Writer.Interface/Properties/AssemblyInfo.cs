241343ea0eb4715e90070a00d5aa6a0769f3e177
mrpetro <petro.silowy@gmail.com>
2021-12-11 19:21:42 +0100

    Converting OpenBreed.Writer.Interface/Legacy to use net standard 2.0

--- src/OpenBreed.Writer.Interface/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("OpenBreed.Writer.Interface")]
-[assembly: AssemblyDescription("")]
-[assembly: ComVisible(false)]
-[assembly: Guid("0ad35c65-4cf1-4f80-8719-714cf4133166")]
