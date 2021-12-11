aa181fb6b62e4fb972a6412ac7d730ca93e72146
mrpetro <petro.silowy@gmail.com>
2021-12-11 19:15:05 +0100

    Converting OpenBreed.Reader.Interface/Legacy to use net standard 2.0

--- src/OpenBreed.Reader.Interface/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("OpenBreed.Reader.Interface")]
-[assembly: AssemblyDescription("")]
-[assembly: ComVisible(false)]
-[assembly: Guid("cfdcf7e3-8ea1-4d17-8b16-c6be157fe87c")]
