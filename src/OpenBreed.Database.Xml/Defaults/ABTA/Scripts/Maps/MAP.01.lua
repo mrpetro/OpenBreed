e3cf4e8a100591dc13c00495afe178a50e8d3381
mrpetro <petro.silowy@gmail.com>
2022-05-14 09:52:29 +0200

    Working on scripting system (WIP)

---
+++ src/OpenBreed.Database.Xml/Defaults/ABTA/Scripts/Maps/MAP.01.lua
@@ -0,0 +1,21 @@
+﻿
+WorldLoaded = function(world)
+
+	Logging:Info("Loaded")
+    
+	ts = TimeSpan.FromSeconds(2)
+
+	Logging:Info(ts:ToString())
+
+	Triggers:AfterDelay(ts, function()
+		Logging:Info("Triggered!!!!!!!!!!!!")
+	end)
+
+    soundId = Sounds:GetByName("Vanilla/Common/Speech/Nash")
+	Sounds:PlaySample(soundId)
+    soundId = Sounds:GetByName("Vanilla/Common/Speech/IsDead")
+	Sounds:PlaySample(soundId)
+    soundId = Sounds:GetByName("Vanilla/Common/Speech/YoureOnYourOwn")
+	Sounds:PlaySample(soundId)
+end
+
