1fe5048cd041ecaba62f8904bdf7b079fab77b90
mrpetro <petro.silowy@gmail.com>
2022-06-10 23:07:08 +0200

    Working on landmines

---
+++ src/OpenBreed.Database.Xml/Defaults/ABTA/Scripts/L1/LandMineExplode.lua
@@ -0,0 +1,18 @@
+﻿local mineEntity, actorEntity = ...
+
+Logging:Info("MineEntityId:" .. tostring(mineEntity.Id))
+Logging:Info("ActorEntityId:" .. tostring(actorEntity.Id))
+
+local stampId = Stamps:GetByName("Vanilla/L1/MineCrater").Id;
+
+mineEntity:PutStamp(stampId, 0)
+
+local soundId = Sounds:GetByName("Vanilla/Common/LandMine/Explosion")
+local duration = Sounds:GetDuration(soundId)
+mineEntity:EmitSound(soundId)	
+
+mineEntity:Destroy()
+Logging:Info("Boom!")
+
+
+
