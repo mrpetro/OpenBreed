dcdd4e592738db576bdfea5e6f5aed7ce29bee64
mrpetro <petro.silowy@gmail.com>
2022-06-12 23:34:09 +0200

    Replacing PickableItem state machines with Lua scripting

---
+++ src/OpenBreed.Database.Xml/Defaults/ABTA/Scripts/Common/ItemPickup.lua
@@ -0,0 +1,25 @@
+﻿local itemEntity, actorEntity = ...
+
+local metaData = itemEntity:GetMetadata()
+
+Logging:Info("ItemEntityId:" .. tostring(itemEntity.Id))
+Logging:Info("ActorEntityId:" .. tostring(actorEntity.Id))
+
+local stampName = metaData.Level .. "/" .. metaData.Name .. "/" .. metaData.Flavor .. "/Picked"
+local soundName = "Vanilla/Common/" .. metaData.Name .. "/Picked"
+
+Logging:Info("StampName:" .. tostring(stampName))
+Logging:Info("SoundName:" .. tostring(soundName))
+
+local stampId = Stamps:GetByName(stampName).Id;
+itemEntity:PutStamp(stampId, 0)
+
+local soundId = Sounds:GetByName(soundName)
+itemEntity:EmitSound(soundId)	
+
+--actorEntity:GiveItem(itemId, 1);
+
+itemEntity:Destroy()
+
+
+
