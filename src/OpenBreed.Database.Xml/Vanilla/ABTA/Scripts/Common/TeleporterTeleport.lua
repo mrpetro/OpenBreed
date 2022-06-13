17201bea8930d05516086756276ea3e7135397c4
mrpetro <petro.silowy@gmail.com>
2022-06-13 23:46:41 +0200

    Renaming content folder from Defaults to Vanilla

---
+++ src/OpenBreed.Database.Xml/Vanilla/ABTA/Scripts/Common/TeleporterTeleport.lua
@@ -0,0 +1,29 @@
+﻿local teleportEntity, actorEntity = ...
+
+Logging:Info("TeleportEntityId:" .. tostring(teleportEntity.Id) .. "(" .. teleportEntity.Tag .. ")")
+Logging:Info("ActorEntityId:" .. tostring(actorEntity.Id) .. "(" .. actorEntity.Tag .. ")")
+
+if (tostring(actorEntity.State) == "Teleporting")
+then
+	return
+end
+
+actorEntity.State = "Teleporting"
+
+local cameraEntity = actorEntity:GetPlayerCamera(Entities)
+
+if (cameraEntity == nil)
+then
+	return
+end
+
+local cameraFadeOutClipId = Clips:GetByName("Vanilla/Common/Camera/Effects/FadeOut").Id;
+local cameraFadeInClipId = Clips:GetByName("Vanilla/Common/Camera/Effects/FadeIn").Id;
+local targetWorldId = actorEntity.WorldId;
+
+
+
+Logging:Info("Teleport!")
+
+
+
