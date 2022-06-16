f6f99b6c93d37ecaa5129521c5cd71f4a9fb8cd8
mrpetro <petro.silowy@gmail.com>
2022-06-17 00:40:22 +0200

    Cleanup

--- src/OpenBreed.Database.Xml/Vanilla/ABTA/Scripts/Common/TeleporterTeleport.lua
+++ src/OpenBreed.Database.Xml/Vanilla/ABTA/Scripts/Common/TeleporterTeleport.lua
@@ -1,7 +1,69 @@
-﻿local teleportEntity, actorEntity = ...
+﻿-- Variable definitions
+local teleportEntity, actorEntity = ...
+local cameraEntity
+local cameraFadeOutClipId;
+local cameraFadeInClipId;
 
-Logging:Info("TeleportEntityId:" .. tostring(teleportEntity.Id) .. "(" .. teleportEntity.Tag .. ")")
-Logging:Info("ActorEntityId:" .. tostring(actorEntity.Id) .. "(" .. actorEntity.Tag .. ")")
+--functions
+local PauseWorld
+local FadeOut
+local SetPosition
+local WorldUnpause
+local FadeIn
+
+-- Functions
+
+PauseWorld = function()
+    Logging:Info("PauseWorld...")
+
+	Triggers:OnPausedWorld(
+        cameraEntity,
+        FadeOut,
+        true)
+
+    cameraEntity:PauseWorld()
+end
+
+FadeOut = function(entity, args)
+    Logging:Info("FadeOut...")
+
+    Triggers:OnEntityAnimFinished(
+        cameraEntity,                      
+        SetPosition,
+        true)
+
+    cameraEntity:PlayAnimation(0, cameraFadeOutClipId)
+end
+
+SetPosition = function(entity, args)
+    Logging:Info("SetPosition...")
+
+    actorEntity:SetPosition(Entities, Shapes, teleportEntity)
+
+    WorldUnpause()
+end
+
+WorldUnpause = function()
+    Logging:Info("WorldUnpause...")
+
+	Triggers:OnUnpausedWorld(
+        cameraEntity,
+        FadeIn,
+        true)
+
+    cameraEntity:UnpauseWorld()
+end
+
+FadeIn = function(entity, args)
+    Logging:Info("FadeIn...")
+
+    cameraEntity:PlayAnimation(0, cameraFadeInClipId)
+end
+
+-- Execution
+
+--Logging:Info("TeleportEntityId:" .. tostring(teleportEntity.Id) .. "(" .. teleportEntity.Tag .. ")")
+--Logging:Info("ActorEntityId:" .. tostring(actorEntity.Id) .. "(" .. actorEntity.Tag .. ")")
 
 if (tostring(actorEntity.State) == "Teleporting")
 then
@@ -10,20 +72,23 @@ end
 
 actorEntity.State = "Teleporting"
 
-local cameraEntity = actorEntity:GetPlayerCamera(Entities)
+cameraEntity = actorEntity:GetPlayerCamera(Entities)
 
 if (cameraEntity == nil)
 then
 	return
 end
 
-local cameraFadeOutClipId = Clips:GetByName("Vanilla/Common/Camera/Effects/FadeOut").Id;
-local cameraFadeInClipId = Clips:GetByName("Vanilla/Common/Camera/Effects/FadeIn").Id;
-local targetWorldId = actorEntity.WorldId;
+
+cameraFadeOutClipId = Clips:GetByName("Vanilla/Common/Camera/Effects/FadeOut").Id;
+cameraFadeInClipId = Clips:GetByName("Vanilla/Common/Camera/Effects/FadeIn").Id;
+
+PauseWorld(); 
 
 
 
-Logging:Info("Teleport!")
+
+
 
 
 
