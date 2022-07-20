e8890f339c4a8e88a75f93ce860a9ec6be123e12
mrpetro <petro.silowy@gmail.com>
2022-07-20 23:40:00 +0200

    Updating LandMineExplode script with destroying near entities

--- src/OpenBreed.Database.Xml/Vanilla/ABTA/Scripts/Common/LandMineExplode.lua
+++ src/OpenBreed.Database.Xml/Vanilla/ABTA/Scripts/Common/LandMineExplode.lua
@@ -11,7 +11,23 @@ local soundId = Sounds:GetByName("Vanilla/Common/LandMine/Explosion")
 local duration = Sounds:GetDuration(soundId)
 mineEntity:EmitSound(soundId)	
 
-mineEntity:Destroy()
+
+
+for i = -1,1,1 
+do
+	for j = -1,1,1 
+	do
+		local nextDoorCell = mineEntity:GetEntityByDataGrid(Worlds, i, j)
+
+		if(nextDoorCell ~= nil)
+		then
+			nextDoorCell:Destroy()
+		end
+
+	end
+end
+
+--mineEntity:Destroy()
 Logging:Info("Boom!")
 
 
