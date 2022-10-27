868f5543659a91c94658d18483c4e97c1883b316
mrpetro <petro.silowy@gmail.com>
2022-10-27 20:20:51 +0200

    Working on scripting

---
+++ src/OpenBreed.Database.Xml/Vanilla/Common/Scripts/Hud/FpsCounter.lua
@@ -0,0 +1,27 @@
+﻿
+EntityTypes.FpsCounter = {}
+
+EntityTypes.FpsCounter.Initialize = function(entity)
+
+    local UpdateFpsCounterPos = function(v, a)
+
+		Logging:Info(tostring(a))
+
+	    Logging:Info("Set FPSPosition")
+		entity:SetPosition(-a.Width / 2.0, -a.Height / 2.0)
+    end
+
+	local hudViewport = Entities:GetHudViewport()
+
+	Triggers:OnEntityViewportResized(hudViewport, UpdateFpsCounterPos);
+	Logging:Info("Initialize")
+
+end
+
+EntityTypes.FpsCounter.UpdateText = function(entity)
+
+	local fps = Rendering.Fps
+	local text = "FPS: " .. string.format("%.2f", fps)
+
+	entity:SetText(0, text)
+end
