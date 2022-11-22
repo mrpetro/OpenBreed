8a584a0dc2657ef81a9b8bb0a4e9c6e7ff6e6e08
mrpetro <petro.silowy@gmail.com>
2022-11-22 21:52:19 +0100

    Adding Mission Screen

--- src/OpenBreed.Database.Xml/Vanilla/ABTA/Scripts/Common/Actor.lua
+++ src/OpenBreed.Database.Xml/Vanilla/ABTA/Scripts/Common/Actor.lua
@@ -3,11 +3,15 @@
     local gameCameraEntity = Entities:GetPlayerCamera(actorEntity)
     local missionScreenCameraEntity = Entities:GetMissionScreenCamera()
     local missionScreenTextEntity = Entities:GetMissionScreenText()
+    local missionScreenBackgroundEntity =  Entities:GetMissionScreenBackground()
     local gameWorld = Worlds:GetWorld(actorEntity)
     local missionEntity = Entities:GetMission(gameWorld.Id)
     local hudCameraEntity = Entities:GetHudCamera()
     local hudViewportEntity = Entities:GetHudViewport()
     local gameViewportEntity = Entities:GetGameViewport()
+    local backgroundDarkenClipId = Clips:GetByName("Vanilla/Common/Picture/Effects/Darken").Id
+    local textFadeInClipId = Clips:GetByName("Vanilla/Common/Text/Effects/FadeIn").Id
+    local textFadeOutClipId = Clips:GetByName("Vanilla/Common/Text/Effects/FadeOut").Id
     local cameraFadeOutClipId = Clips:GetByName("Vanilla/Common/Camera/Effects/FadeOut").Id
     local cameraFadeInClipId = Clips:GetByName("Vanilla/Common/Camera/Effects/FadeIn").Id
     local missionMetadata = missionEntity:GetMetadata()
@@ -21,11 +25,18 @@
     local GameWorldPause
     local GameWorldFadeOut
     local SwitchViewToMissionWorld
-    local MissionWorldShowText
-    local MissionWorldFadeOut
+
+    local ScreenFadeIn
+    local Wait3Seconds
+    local DarkenBackground
+    local TextFadeIn
+    local WaitForKey
+    local TextFadeOut
+    local ScreenFadeOut
+
     local SwitchViewToGameWorld
     local GameWorldUnpause
-    local DisplayTextWithNextCharacter
+    local WaitForAction
     local ShowAllTextIfNeeded
 
     -- Functions
@@ -64,66 +75,73 @@
         missionScreenCameraEntity:SetBrightness(0)
         hudViewportEntity:SetViewportCamera(missionScreenCameraEntity.Id)
 
+        ScreenFadeIn()
+    end
+
+    ScreenFadeIn = function()
+        Logging:Info("Screen Fade In...")
+
         Triggers:OnEntityAnimFinished(
             missionScreenCameraEntity,                      
-            MissionWorldShowText,
+            Wait3Seconds,
             true)
 
         missionScreenCameraEntity:PlayAnimation(0, cameraFadeInClipId)
-
-        Triggers:EveryFrame(
-            Commentator,
-            DisplayTextWithNextCharacter,
-            0,
-            true)
     end
 
-    DisplayTextWithNextCharacter = function(entity, args)
+    Wait3Seconds = function(entity, args)
+        Logging:Info("Wait 2 seconds...")
 
-        if( currentCharacter > textLength)
-        then
-            return
-        end
-
-        local textPart = string.sub(text,0, currentCharacter)
-
-        missionScreenTextEntity:SetText(0, textPart)
-
-        currentCharacter = currentCharacter + 1
-
-        Triggers:EveryFrame(
+	    Triggers:AfterDelay(
             Commentator,
-            DisplayTextWithNextCharacter,
-            0,
-            true)
+            TimeSpan.FromMilliseconds(2000),
+            DarkenBackground)
     end
 
-    MissionWorldShowText = function(entity, args)
-        Logging:Info("Mission world show text...")
+    DarkenBackground = function()
+        Logging:Info("Darken background...")
+
+        Triggers:OnEntityAnimFinished(
+            missionScreenBackgroundEntity,                      
+            TextFadeIn,
+            true)
+
+        missionScreenBackgroundEntity:PlayAnimation(0, backgroundDarkenClipId)
+    end
+
+    TextFadeIn = function(entity, args)
+        Logging:Info("Text fade in...")
+
+        Triggers:OnEntityAnimFinished(
+            missionScreenTextEntity,                      
+            WaitForKey,
+            true)
+
+        missionScreenTextEntity:PlayAnimation(0, textFadeInClipId)
+    end
+
+    WaitForKey = function(entity, args)
+        Logging:Info("Wait for key...")
 
         Triggers:AnyKeyPressed(
-            ShowAllTextIfNeeded,
+            TextFadeOut,
             true)
+
     end
 
-    ShowAllTextIfNeeded = function(args)
+    TextFadeOut = function()
+        Logging:Info("Text fade out...")
 
-        --If all text is shown, go straight to fade out
-        if( currentCharacter > textLength)
-        then
-            MissionWorldFadeOut(nil)
-            return
-        --Else show all text and wait for button to be pressed
-        else
-            currentCharacter = textLength
-            Triggers:AnyKeyPressed(
-                MissionWorldFadeOut,
-                true)
-        end
+        Triggers:OnEntityAnimFinished(
+            missionScreenTextEntity,                      
+            ScreenFadeOut,
+            true)
+
+        missionScreenTextEntity:PlayAnimation(0, textFadeOutClipId)
     end
 
-    MissionWorldFadeOut = function(args)
-        Logging:Info("Mission world fade out...")
+    ScreenFadeOut = function(entity, args)
+        Logging:Info("Screen Fade Out...")
 
         Triggers:OnEntityAnimFinished(
             missionScreenCameraEntity,                      
@@ -133,7 +151,8 @@
         missionScreenCameraEntity:PlayAnimation(0, cameraFadeOutClipId)
     end
 
-    GameWorldUnpause = function()
+
+    GameWorldUnpause = function(entity, args)
         Logging:Info("Game world unpause...")
 
 	    Triggers:OnUnpausedWorld(
@@ -167,6 +186,9 @@
 	    return
     end
 
+    missionScreenBackgroundEntity:SetPictureColor(1.0,1.0,1.0,1.0)
+    missionScreenTextEntity:SetTextColor(0,1.0,1.0,1.0,0.0)
+    missionScreenTextEntity:SetText(0, text)
     GameWorldPause()
 end
 
