71b11fb2d1eed5610b8c5731291e77638c1ab47a
mrpetro <petro.silowy@gmail.com>
2023-01-09 00:02:15 +0100

    Adding CursorScript

---
+++ src/OpenBreed.Database.Xml/Vanilla/Common/Scripts/Hud/Cursor.lua
@@ -0,0 +1,18 @@
+﻿local function onUpdate(entity, dt)
+
+	local pos = entity:GetPosition()
+	local text = "(" .. string.format("%.0f", pos.X) .. ", " .. string.format("%.0f", pos.Y) .. ")"
+	entity:SetText(0, text)
+
+end
+
+local function onInit(entity)
+
+end
+
+return {
+    systemHooks = {
+        onUpdate = onUpdate,
+        onInit = onInit
+    }
+}
