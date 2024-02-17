df7b0d9c2393bc645577c285a6a9dbb3d48c84f0
mrpetro <24554305+mrpetro@users.noreply.github.com>
2024-02-17 22:55:13 +0100

    (WIP) Working on turrets

--- data/Vanilla/ABTA/Scripts/L1/Turret.lua
+++ data/Vanilla/ABTA/Scripts/L1/Turret.lua
@@ -5,8 +5,8 @@ local function Explode(entity)
     local pos = entity:GetPosition()
     entity:StartEmit("ABTA\\Templates\\Common\\Projectiles\\Explosion")
         :SetOption("flavor", "Big")
-        :SetOption("startX", pos.X + 16 + 8)
-        :SetOption("startY", pos.Y + 16 + 8)
+        :SetOption("startX", pos.X)
+        :SetOption("startY", pos.Y)
         :Finish()
      
     Worlds:RequestRemoveEntity(entity)
@@ -14,13 +14,37 @@ local function Explode(entity)
 		
 end
 
+local function OnDirectionChanged(entity, args)
+
+    local pos = entity:GetPosition()
+    local targetDirection = entity:GetTargetDirection()
+    local direction = entity:GetDirection()
+	
+	--Logging:Info("TURRET.POS: (" .. tostring(pos.X) .. ", " .. tostring(pos.Y) .. ")" )
+
+	--Logging:Info("TURRET.DIR : (" .. tostring(direction.X) .. ", " .. tostring(direction.Y) .. ")" )
+
+
+    local degree = MovementTools.SnapToCompass16Degree(direction.X, direction.Y)
+    local animName = "Vanilla/L1/Turret/Tracking/" .. tostring(degree)
+   
+    local clip = Clips:GetByName(animName)
+
+    entity:PlayAnimation(0, clip.Id)
+end
+
 local function OnInit(entity)
+
+    Triggers:OnEntityDirectionChanged(
+        entity,
+        OnDirectionChanged,
+        false)
 
     Triggers:OnDestroyed(
         entity,
         Explode,
         false)
-			
+		
 end
 
 return {
