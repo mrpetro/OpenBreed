44c51c6b2a3690da026fa529592bd75d3eb61e40
mrpetro <24554305+mrpetro@users.noreply.github.com>
2024-02-28 22:11:37 +0100

    Adding ComponentsMan for future usage

--- src/OpenBreed.Wecs/Components/ComponentsMan.cs
+++ src/OpenBreed.Wecs/Components/ComponentsMan.cs
@@ -1,51 +1,112 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OpenBreed.Wecs.Components
 {
-    public class ComponentsMan
+    public class ComponentsMan : IComponentsMan
     {
-        private Dictionary<Type, Dictionary<int, IEntityComponent>> components = new Dictionary<Type, Dictionary<int, IEntityComponent>>();
+        #region Private Fields
 
-        public void AddEntityComponent<T>(int entityId, IEntityComponent component) where T : class
+        private readonly Dictionary<Type, Dictionary<int, IEntityComponent>> components = new Dictionary<Type, Dictionary<int, IEntityComponent>>();
+        private readonly Dictionary<int, Dictionary<Type, IEntityComponent>> types = new Dictionary<int, Dictionary<Type, IEntityComponent>>();
+
+        #endregion Private Fields
+
+        #region Public Methods
+
+        public void Add<TComponent>(int entityId, IEntityComponent component) where TComponent : class
         {
             Dictionary<int, IEntityComponent> typeComponents = null;
 
-            if (!components.TryGetValue(typeof(T), out typeComponents))
+            if (!components.TryGetValue(typeof(TComponent), out typeComponents))
             {
                 typeComponents = new Dictionary<int, IEntityComponent>();
-                components.Add(typeof(T), typeComponents);
+                components.Add(typeof(TComponent), typeComponents);
             }
 
             typeComponents.Add(entityId, component);
         }
 
-        public bool RemoveEntityComponent<T>(int entityId)
+        public bool Set<TComponent>(int id, TComponent component) where TComponent : class, IEntityComponent
+        {
+            var result = false;
+            if (!components.TryGetValue(typeof(TComponent), out Dictionary<int, IEntityComponent> typeComponents))
+            {
+                typeComponents = new Dictionary<int, IEntityComponent>();
+                components.Add(typeof(TComponent), typeComponents);
+                result = true;
+            }
+
+            if (!typeComponents.ContainsKey(id))
+            {
+                result = true;
+            }
+
+            typeComponents[id] = component;
+
+            return result;
+        }
+
+        public bool Contains<TComponent>(int entityId)
+        {
+            if (!components.TryGetValue(typeof(TComponent), out Dictionary<int, IEntityComponent> typeComponents))
+            {
+                return false;
+            }
+
+            return typeComponents.ContainsKey(entityId);
+        }
+
+        public bool TryGet<TComponent>(int entityId, out TComponent component) where TComponent : class
+        {
+            if (!components.TryGetValue(typeof(TComponent), out Dictionary<int, IEntityComponent>  typeComponents))
+            {
+                component = default;
+                return false;
+            }
+
+            if (!typeComponents.TryGetValue(entityId, out IEntityComponent cmp))
+            {
+                component = default;
+                return false;
+            }
+
+            component = (TComponent)cmp;
+            return true;
+        }
+
+        public TComponent Get<TComponent>(int entityId)
+        {
+            if (!components.TryGetValue(typeof(TComponent), out Dictionary<int, IEntityComponent> typeComponents))
+                return default;
+
+            IEntityComponent component;
+
+            if (!typeComponents.TryGetValue(entityId, out component))
+                return default;
+
+            return (TComponent)component;
+        }
+
+        public bool Remove<TComponent>(int entityId)
         {
             Dictionary<int, IEntityComponent> typeComponents = null;
 
-            if (!components.TryGetValue(typeof(T), out typeComponents))
+            if (!components.TryGetValue(typeof(TComponent), out typeComponents))
                 return false;
 
             return typeComponents.Remove(entityId);
         }
 
-        public T GetEntityComponent<T>(int entityId) where T : class
+        public ICollection<Type> Types(int id)
         {
-            Dictionary<int, IEntityComponent> typeComponents = null;
-
-            if (!components.TryGetValue(typeof(T), out typeComponents))
-                return null;
-
-            IEntityComponent component;
-
-            if (!typeComponents.TryGetValue(entityId, out component))
-                return null;
-
-            return (T)component;
+            throw new NotImplementedException("This needs proper implementation.");
         }
+
+        #endregion Public Methods
     }
 }
---
+++ src/OpenBreed.Wecs/Components/IComponentsMan.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace OpenBreed.Wecs.Components
+{
+    public interface IComponentsMan
+    {
+        #region Public Methods
+
+        void Add<TComponent>(int entityId, IEntityComponent component) where TComponent : class;
+
+        bool Set<TComponent>(int id, TComponent component) where TComponent : class, IEntityComponent;
+
+        bool Contains<TComponent>(int entityId);
+
+        bool TryGet<TComponent>(int entityId, out TComponent component) where TComponent : class;
+
+        T Get<T>(int entityId);
+
+        bool Remove<T>(int entityId);
+
+        ICollection<Type> Types(int id);
+
+        #endregion Public Methods
+    }
+}
